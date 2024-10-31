d1b90ead28c8850ca502b10bd99f9b020d77d113
Josué Bayidikila <josue-dev@outlook.fr>
2024-10-31 12:47:45 +0100

    feat(STOCK-6): update config mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'stockbuster',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT, 10),
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
       synchronize: true, // for development purposes only, in production it should be set to false
     }),
   ],
